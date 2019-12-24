d7cde6980329098abcca7ffb19e8ab8240b8782e
Merk <steffen.merk@sap.com>
2019-12-24 14:39:53 +0100

    Migrated new Kanal app

---
+++ kanal2020-app/src/app/app.component.ts
@@ -0,0 +1,99 @@
+import { Component } from '@angular/core';
+import { Kanal } from './Kanal';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Rund auf Eckig';
+
+  public kanal: Kanal;
+  private a: number;
+  private b: number;
+  private d: number;
+
+  public hint_e: number;
+  public hint_f: number;
+
+  public displayedColumns: string[] = ['column1', 'column2', 'column3', 'column4'];
+
+  private DEFAULT_VALIDATOR_ARRAY = [
+    Validators.required,
+    Validators.minLength(1),
+    Validators.maxLength(4)];
+
+  kanalForm = this.formBuilder.group({
+    a: ['', this.DEFAULT_VALIDATOR_ARRAY],
+    b: ['', this.DEFAULT_VALIDATOR_ARRAY],
+    d: ['', this.DEFAULT_VALIDATOR_ARRAY],
+    l: ['', this.DEFAULT_VALIDATOR_ARRAY],
+    dicke: ['', this.DEFAULT_VALIDATOR_ARRAY],
+    e: ['', this.DEFAULT_VALIDATOR_ARRAY],
+    f: ['', this.DEFAULT_VALIDATOR_ARRAY],
+  });
+
+  /*
+  e = (d - b) / 2;
+  f = (d - a) / 2;
+  */
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  public resetForm(): void {
+    this.kanalForm.reset();
+    this.kanal = undefined;
+  }
+
+  public ngOnInit(): void {
+    this.kanalForm.get('a').valueChanges.subscribe(
+      (value) => {
+        this.a = parseInt(value, 10);
+        if (this.a && this.d) {
+          this.calculateHintF();
+        }
+      });
+    this.kanalForm.get('b').valueChanges.subscribe(
+      (value) => {
+        this.b = parseInt(value, 10);
+        if (this.b && this.d) {
+          this.calculateHintE();
+        }
+      });
+    this.kanalForm.get('d').valueChanges.subscribe(
+      (value) => {
+        this.d = parseInt(value, 10);
+        if (this.b) {
+          this.calculateHintE();
+        }
+        if (this.a) {
+          this.calculateHintF();
+        }
+      });
+  }
+
+  private calculateHintE(): void {
+    this.hint_e = (this.d - this.b) / 2;
+  }
+
+  private calculateHintF(): void {
+    this.hint_f = (this.d - this.a) / 2;
+  }
+
+  public onSubmit(): void {
+    this.kanal = new Kanal(
+      this.kanalForm.controls.a.value,
+      this.kanalForm.controls.b.value,
+      this.kanalForm.controls.d.value,
+      this.kanalForm.controls.l.value,
+      this.kanalForm.controls.dicke.value,
+      this.kanalForm.controls.e.value,
+      this.kanalForm.controls.f.value);
+  }
+
+
+}
---
+++ kanal2020-app/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+// angular material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatButtonModule,    
+    MatInputModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
